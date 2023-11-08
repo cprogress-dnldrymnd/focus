5b5f31c2e8e96e0dabaa8f53af24c9c3f0ccce41
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-09 00:13:02 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,9 +1,11 @@
 <?php
-function resource_filters() {
+function resource_filters()
+{
     ob_start();
-    ?>
+    $categories = get_categories();
+?>
     <div class="filters-holder">
-    <div class="filter-item">
+        <div class="filter-item">
             <div class="filter-header">
                 <span>Filter by</span>
                 <span>Reset All</span>
@@ -18,11 +20,15 @@ function resource_filters() {
                 <span>Reset All</span>
             </div>
             <div class="filter-body">
-                <input type="text" name="s" id="search" placeholder="Search…">
+                <div class="category-fitler">
+                    <?php foreach ($categories as $categ) { ?>
+                        <a href="<?= get_term_link($categ->term_id) ?>"><?= $categ->name ?></a>
+                    <?php } ?>
+                </div>
             </div>
         </div>
     </div>
-    <?php
+<?php
     return ob_get_clean();
 }
 
