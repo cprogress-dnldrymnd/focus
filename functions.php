1a594b4ddd1fe70b59f1dd5ffaa39a258ae1d8c0
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-09 18:48:51 +0800

    css

--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php
 
-function child_styles() {
-	wp_enqueue_style( 'my-child-theme-style', get_stylesheet_directory_uri() . '/style.css', array( 'vamtam-front-all' ), false, 'all' );
+function child_styles()
+{
+	wp_enqueue_style('my-child-theme-style', get_stylesheet_directory_uri() . '/style.css', array('vamtam-front-all'), false, 'all');
 }
-add_action( 'wp_enqueue_scripts', 'child_styles', 11 );
+add_action('wp_enqueue_scripts', 'child_styles', 11);
 
 /*-----------------------------------------------------------------------------------*/
 /* Register Carbofields
@@ -15,3 +16,17 @@ function tissue_paper_register_custom_fields()
 }
 
 require_once('includes/shortcodes.php');
+
+function action_wp_footer()
+{
+?>
+	<script>
+		jQuery(document).ready(function() {
+			$header_height = jQuery('div[data-elementor-type="header"]').outerHeight();
+			jQuery('body').css('--header-height', $header_height + 'px');
+		});
+	</script>
+<?php
+}
+
+add_action('wp_footer', 'action_wp_footer');
