649e4ab8231c5d1a32c1d5290d60ce41c82b8908
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-11-09 19:17:41 +0800

    css

--- functions.php
+++ functions.php
@@ -24,6 +24,7 @@ function action_wp_footer()
 		jQuery(document).ready(function() {
 			$header_height = jQuery('div[data-elementor-type="header"]').outerHeight();
 			jQuery('body').css('--header-height', $header_height + 'px');
+			jQuery('body').css('--header-height-negative', '-' + $header_height + 'px');
 		});
 	</script>
 <?php
