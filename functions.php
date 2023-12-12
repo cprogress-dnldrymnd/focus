0ab4192e890730f96ef158992d0a43bbe5799869
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-12 20:05:09 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -53,10 +53,8 @@ function action_wp_footer()
 
 		function fixed_header_bg_mobile() {
 			setTimeout(function() {
-				if (window.innerWidth > 991) {
-					$height = jQuery('div[data-elementor-post-type="page"] > *:first-child').outerHeight();
-					jQuery("#top-bg").css('height', $header_height + "px");
-				}
+				$height = jQuery('div[data-elementor-post-type="page"] > *:first-child').outerHeight();
+				jQuery("#top-bg").css('height', $header_height + "px");
 			}, 300);
 
 		}
